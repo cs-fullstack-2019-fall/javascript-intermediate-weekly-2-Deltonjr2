d82508b130b63d88140ae89c10d58be8ed5eca33
Deltonjr2 <Demetria.Farewell@yahoo.com>
2019-08-16 16:23:18 -0500

    project 2

---
+++ index.js
@@ -0,0 +1,100 @@
+  // alert  ("test");
+//
+// / // Build a Simple Quiz test Program
+// Weekly Project (Total 10 points)
+// //
+// // Create a program that will let you create a simple question generator.
+// //
+// //     The program will use both the JavaScript console and the browser. The questions/answers should be displayed in the JavaScript console and answer 'prompt' should be used to accept answer input from the user.
+// //
+// //     Sample Question
+// //
+// // Phase 1: Ask 1 question only then exit
+// //
+// // Create a Class to represent an individual Question (4 points).
+// //
+// // Your Question class should have 3 properties:
+// //
+// //     Question Text - This is the actual text of the given question
+// // Answers - This should be an array containing the available answers for the question. The number of answers available for a given question is up to the Developer.
+// //     Correct Answer Index - This integer should be the index of the correct answer in your answer array.
+// //     Your Question Class should have at least 2 methods:
+// //
+// //     A method to output the question and the available answers.
+// //     A method that accepts an integer, and checks if the value matches the Correct Answer Index.
+// //     Each answer should have a number displayed that the User can enter to pick an answer (using the Question class method you implemented) (2 points).
+// //
+// // // Refer to image. (HINT: Use 0 - however many answers and use a Template String or simply concatenate the corresponding answer (+) to make a numbered answer in the console).
+// //
+// // Create 3 or more Question instances via the Question class constructor (1 point).
+// //
+// // Create question instances manually in your JavaScript code. An example of creating a new Question should look something like this (NOTE: Passing in the values for the 3 properties of the Question class to its Constructor):
+// //
+// // let q1 = new Question('How many million miles is Earth from the Sun?',['123', '93', '150'], 1);
+// //
+// // Load each question that you make/instantiate into an Available Question List array using any array method you choose to add the Questions as new elements in the array (1 point).
+// //
+// // Select one random question from your Available Question List array and log it to the console (0 points).
+// //
+// // Your Question class should be able to output the question and the available answers (refer to image above). Use this code or similar to generate a random index number to use to get a Question from your Available Question List array:
+// //
+// // // Pick a random question (CODE PROVIDED TO STUDENTS FOR RANDOM NUMBER GENERATION)
+// //     let index_of_a_question = Math.floor(Math.random() * available_question_list_array.length);
+// // Prompt the User to enter the number of what they think the correct answer is (1 point).
+// //
+// // The user should input the corresponding number of the correct answer displayed in the JavaScript console.
+// //
+// //     Use your Question Class method that checks for a correct answer (1 point).
+// //
+// // Check if the User’s answer is correct and display an alert stating if they were right or not.
+// //
+// //     Sample Response to Incorrect Answer
+// //
+// // Challenges
+// //
+// // Enhance the solution so that it will let the User keep answering questions until ‘q’ is entered (Bonus: handle any case lower/or upper that may be entered by the User).
+// // Track the User’s score. Each time the answer was correct, give them a point. Display the running total of the numbers of questions they got right in the console before or after the question(s).
+// //     Make sure if the user enters an invalid answer number say "Invalid Answer Number. Please Try Again!", then retry the question or give them a new one.
+// // Notes
+// //
+// // It's expected you know how to use git to push and pull your projects. You will not get ANY points if you push your work after 5pm.
+//
+class Car{
+
+Constructor (color, make, modelyear){
+
+  this.color_prop=color-color_prop;
+  this.make_prop= make_prop;
+  this.modelyear_prop=modelyear_prop;
+  this.enginePowerType_prop= 10;
+//10 cylinder
+}
+//This method returns the engine speed of the car}
+
+  return:    this.enginePowerType_prop;
+}
+
+// Create a car instance
+  {
+let myCar = new ["Red, Cadillac, 1950"];
+let myCar = new ["Silver, Mercedes, 1995"];
+let myCar = new ["Gold, Hummer, 2015];
+
+    {
+
+// Instances
+// Change the EnginePower for one instance of the car
+
+}
+
+console.log (myCar.enginePower);
+console.log (myCar.enginePowerType_prop=6(); //Original 6 cylinder Engine Power
+console.log (myCarmyCar.enginePower); //Change enginePower)
+
+// Change the color of the car
+console.log ("myCar Color is" + myCar.color_prop);
+console.log ("yourCar Color is" + yourCar.color_prop);
+console.log ("their car color is"+ theirCar.color_prop);
+
+myCar.color_prep="Orange";
+Console.log ("Color)is "+ myCar
